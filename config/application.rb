b08ea9e9950104d203ee5c87c53605876777f4b9
toshi-oliver <igpoid@gmail.com>
2018-09-11 00:54:53 +0900

    edit config
    不要なファイルを生成しないよう設定を変更する

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GitApp
+module gymmap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
   end
 end
