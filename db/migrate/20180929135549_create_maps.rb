0b3264871e8b7a6f5a638a4c677ed5527f98da8a
toshi-oliver <igpoid@gmail.com>
2018-10-02 10:13:14 +0900

    Edit the migration_file

--- db/migrate/20180929135549_create_maps.rb
+++ db/migrate/20180929135549_create_maps.rb
@@ -2,11 +2,7 @@ class CreateMaps < ActiveRecord::Migration[5.2]
   def change
     create_table :maps do |t|
       t.string      :name, null: false
-      t.reference   :category_id, foreign_key: true, null: false
-      t.reference   :place_id, foreign_key: true, null: false
-      t.reference   :user_id, foreign_key: true, null: false
-      t.content     :text
-      t.text        :image, unique: true
+
       t.timestamps
     end
   end
