01ebc9161a4263e4f196a81ac39c0a450a85e872
toshi-oliver <igpoid@gmail.com>
2018-10-02 10:34:30 +0900

    difine new and create and map_params action in  the maps_controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,8 +5,22 @@ class MapsController < ApplicationController
   def index
   end
   
+  def new
+    @map = Map.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @map = Map.create(map_params)
+  end
+
   private
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  def map_params
+    params.required(:map).permit(:name, :text, :imgae, categories_attributes: [:id], places_attributes: [:id]).merge(user_id: current_user.id)
+  end 
 end
+ 
