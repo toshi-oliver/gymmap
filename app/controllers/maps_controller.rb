560e9f5fdd4c2879ff175f1bcbfac9753caeca45
toshi-oliver <igpoid@gmail.com>
2018-10-08 21:45:32 +0900

    add the search_instance to the maps_cotroller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -13,6 +13,10 @@ class MapsController < ApplicationController
     @map = Map.create(map_params)
   end
 
+  def search
+    @maps = Map.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  end
+
   private
   def move_to_index
     redirect_to action: :index unless user_signed_in?
