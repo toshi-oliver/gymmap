0d0fa1813e5aeb7d5010e80b6e3c2ec5f2de2343
toshi-oliver <igpoid@gmail.com>
2018-10-18 03:39:00 +0900

    delete acts_as_paranoid  from user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  acts_as_paranoid
   has_many :maps, dependent: :destroy
 end
