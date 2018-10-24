a79808149370583295d260c91f56a7f639c3f21a
toshi-oliver <igpoid@gmail.com>
2018-10-24 23:33:31 +0900

    add the assosiation to the user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :maps, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
