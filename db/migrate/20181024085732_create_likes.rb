fa463450b89bb34c689807c55a8ba8beb8a97215
toshi-oliver <igpoid@gmail.com>
2018-10-29 19:09:46 +0900

    edit the migration file
    referencesを設定することで、自動的にindexを設定できる。また、カラム名もuserのままでおk

--- db/migrate/20181024085732_create_likes.rb
+++ db/migrate/20181024085732_create_likes.rb
@@ -1,14 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :map_id
-
+      t.references :user, foreign_key: true
+      t.references :map, foreign_key: true
       t.timestamps
-
-      t.index :user_id
-      t.index :map_id
-      t.index [:user_id, :map_id], unique: true
     end
   end
 end
