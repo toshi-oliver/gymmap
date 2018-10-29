5d6aa3f86658db257656500bd4ebd4542b6974bb
toshi-oliver <igpoid@gmail.com>
2018-10-29 19:03:55 +0900

    add counter_cache to like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :map
+  belongs_to :map, counter_cache: :likes_count
   validates :user_id, presence: true
   validates :map_id, presence: true
 end
